e93cbc7fa3ca2390f489aebe4b4efa502530570b
Marcos Floriano <marcos.floriano@sptech.school>
2024-11-02 19:46:08 -0300

    Mudei a navbar

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,6 +1,8 @@
 import PropTypes from "prop-types";
 import "./Navbar.css";
-import logo from "../../assets/Tech-Tooth-Logo.svg";
+import logo from "../../assets/Tech-Tooth-Logo.png";
+import Button from "../Button/Button";
+import { useState } from "react";
 
 const Navbar = (props) => {
   const [listMenu, setListMenu] = useState(false);
@@ -11,28 +13,74 @@ const Navbar = (props) => {
     } else {
       setListMenu(false);
     }
-  }
+  };
 
   return (
     <>
       <div className="navbar">
-        <div className="box" onClick={() => { window.location.href = '#home'; }}>
-          <img className="logo" src={logo} width={'250px'} />
-          <h1 className="navbar-title">TECH TOOTH</h1>
+        <div className="box">
+          <img className="logo" src={logo} width={"250px"} />
+          <h1 className="navbar-title">Tech Tooth</h1>
+        </div>
+        <ul className="navbar-links">
+          {props.items.map((item, index) => (
+            <li key={index} className="navbar-item">
+              <a href={item.href} className="navbar-link">
+                {item.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <a href="#contato" className="a">
+          <Button
+            className="button"
+            text="Solicitar Demonstração"
+            padding="0.6rem 8%"
+            radius="10px"
+          />
+        </a>
+
+        <div className="mobile">
+          {!listMenu && (
+            <div className="menu" onClick={showOptionsMenu}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                className="bi bi-list iconMenu"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
+                />
+              </svg>
+            </div>
+          )}
         </div>
 
-        <div className="navbar-content">
-          <ul className="navbar-links">
-            {props.items.map((item, index) => (
-              <li key={index} className="navbar-item">
-                <a href={item.href} className="navbar-link">
-                  {item.title}
-                </a>
-              </li>
-            ))}
-          </ul>
-          <Button variant="contained" style={{width: '40%', backgroundColor: '#007bff'}} onClick={() => { window.location.href = '#contato'; }}>Solicitar Demonstração</Button>
-        </div>
+        {listMenu && (
+          <div className="closeBox">
+            <div onClick={showOptionsMenu} className="div">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                className="bi bi-x-lg close"
+                viewBox="0 0 16 16"
+              >
+                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+              </svg>
+            </div>
+            <ul>
+              {props.items.map((item, index) => (
+                <li key={index} className="navbar-item">
+                  <a href={item.href} className="navbar-link">
+                    {item.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     </>
   );
